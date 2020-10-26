27082e3789a7a2f2702ab385c82397008e95f94b
Harun Šaranović <harunsaranovic@users.noreply.github.com>
2020-10-26 18:18:42 +0100

    basic api get methods created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header.js';
+import WhyUs from './components/why-us/WhyUs.js';
+import Networks from './components/Networks.js';
+import Footer from './components/Footer.js';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+
+			<div class="main">
+				<div class="slogan">
+					<h2>Evolution through innovation and visionary technology</h2>
+					<h3>Some space for text with smaller size than the slogan is going to be needed</h3>
+				</div>
+				<div class="stake-mob display-mob">
+					<p class="stake-now" id="mobstake2">
+						<div id="go-net2">
+							Stake Now<i class="fas fa-coins" />
+						</div>
+					</p>
+				</div>
+			</div>
+
+			<Networks />
+			<WhyUs />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
